e9b93cce8d24d32963e61bb1219b022b79dd1c0e
Aurelius Huang <cm.huang@aftership.com>
2023-12-17 16:37:22 +0800

    首页图片调整

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,8 +4,8 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: '理论体系',
-    Svg: require('@site/static/img/knowledge.svg').default,
+    title: '知识体系',
+    Svg: require('@site/static/img/home/knowledge.svg').default,
     description: (
       <>
         构建机器学习、深度学习、AIGC、流行神经网络模型等人工智能基础理论知识体系。
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: '场景应用',
-    Svg: require('@site/static/img/scenarios.svg').default,
+    Svg: require('@site/static/img/home/scenarios.svg').default,
     description: (
       <>
         关注 NLP、搜索引擎、推荐系统、CV 等人工智能领域的应用场景与落地实践。
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: '编程语言',
-    Svg: require('@site/static/img/programing.svg').default,
+    Svg: require('@site/static/img/home/programing.svg').default,
     description: (
       <>
         工程落地过程常用编程语言与服务框架。
