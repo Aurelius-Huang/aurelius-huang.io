53e3403dbb81ae2dba575fd9901ceea52f7c66a5
Aurelius Huang <cm.huang@aftership.com>
2023-12-18 13:21:10 +0800

    开启 Markdown Math 样式，新增 ml articles

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,6 +5,8 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import { themes as prismThemes } from 'prism-react-renderer';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -84,6 +86,10 @@ const config = {
         path: './articles/ml',
         routeBasePath: 'ml',
         sidebarPath: './sidebars.js',
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+        // remarkPlugins: [(await import('remark-math')).default],
+        // rehypePlugins: [(await import('rehype-katex')).default],
       },
     ],
     [
@@ -291,6 +297,15 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
 };
 
 export default config;
